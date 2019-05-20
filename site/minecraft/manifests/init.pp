407bdd7fc5e6745c4a3703d9dbe44a7a055bd0e2
Vicky <xolosno270@gmail.com>
2019-05-19 20:07:16 -0700

    Create init.pp

---
+++ site/minecraft/manifests/init.pp
@@ -0,0 +1,21 @@
+class minecraft {
+file {'/opt/mycraft':
+  ensure => directory,
+ }
+ file { 'opt/minecraft/minecraft_server.jar':
+    ensure => file,
+    source => 'https://launcher.mojang.com/v1/objects/886945bfb2b978778c3a0288fd7fab09d315b25f/server.jar',
+   }
+  package {'java':
+    ensure => present,
+    }
+  file {'/opt/minecraft/eula.txt':
+    ensure => file,
+    content => 'eula=true'
+    }
+  file { '/etc/systemd/system/mycraft.service':
+      ensure => file,
+      source => 'puppet:///modules/minecraft/minecraft.service',
+    }
+ }
+    
