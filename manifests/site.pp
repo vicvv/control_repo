8a9d972cc2faf7be15cf457abfc45e9ad95d6e8b
Vicky <xolosno270@gmail.com>
2019-05-19 18:49:52 -0700

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -2,6 +2,10 @@ node default {
 }
 node 'master.puppet.vm' {
   include role::master_server
+  file { '/root/README':
+    ensure => file,
+    content => $fqdn,
+  }
 }
 node /^web/{
   include role::app_server
@@ -10,3 +14,5 @@ node /^web/{
 node /^db/{
   include role::db_server
 }
+
+
