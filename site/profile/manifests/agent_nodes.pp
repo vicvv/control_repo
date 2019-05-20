335b410cb2ec9796be32f7a688db162603ab091a
Vicky <xolosno270@gmail.com>
2019-05-19 19:18:30 -0700

    Update agent_nodes.pp

--- site/profile/manifests/agent_nodes.pp
+++ site/profile/manifests/agent_nodes.pp
@@ -2,9 +2,12 @@ class profile::agent_nodes{
 include dockeragent
 dockeragent::node{'web.puppet.vm':}
 dockeragent::node{'db.puppet.vm':}
-host {'web.puppet.vm':
-ensure => present,
-ip => '172.18.0.2'
-}
-
+  host {'web.puppet.vm':
+  ensure => present,
+  ip => '172.18.0.2'
+    }
+  host {'web.puppet.vm':
+  ensure => present,
+  ip => '172.18.0.3'
+  }
 }
