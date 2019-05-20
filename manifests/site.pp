a57c62420718d8ca830087e98e0ee84e62ba2398
Vicky <xolosno270@gmail.com>
2019-05-19 20:17:19 -0700

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -7,6 +7,9 @@ node 'master.puppet.vm' {
     content => "welsome to ${fqdn}\n",
   }
 }
+node 'minetest.puppet.vm'{
+  include role::minecraft_server
+}
 node /^web/{
   include role::app_server
 }
